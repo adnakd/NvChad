d81621b8c4dfd783eb3de3e913d0274158dc136f
adnakd <adnak.dev@gmail.com>
2026-04-19 14:53:41 +0530

    hide folders

---
+++ lua/plugins/custom1.lua
@@ -0,0 +1,11 @@
+-- Example: lua/custom/plugins.lua
+return {
+  "nvim-tree/nvim-tree.lua",
+  opts = {
+    filters = {
+      dotfiles = true, -- Hides all dotfiles by default
+      custom = { "^.git$" }, -- Specifically ensures .git is hidden
+    },
+  },
+}
+
---
+++ lua/plugins/custom2.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-telescope/telescope.nvim",
+require('telescope').setup({
+  defaults = {
+    file_ignore_patterns = { "node_modules", ".git/", "build/" },
+  }
+}),
+}
+
